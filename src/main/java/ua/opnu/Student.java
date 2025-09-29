85cd3f8a97ba205c2db65bce10f463a569b20eef
gutigomd <blackbull.od@gmail.com>
2025-09-29 10:38:10 +0300

    lab2 completed

--- src/main/java/ua/opnu/Student.java
+++ src/main/java/ua/opnu/Student.java
@@ -24,6 +24,14 @@ public class Student {
         }
     }
 
+    @SuppressWarnings("unused")
+    public String[] getCourses() {
+        // Повертаємо копію лише заповненої частини масиву
+        String[] activeCourses = new String[coursesCount];
+        System.arraycopy(courses, 0, activeCourses, 0, coursesCount);
+        return activeCourses;
+    }
+
     public int getCourseCount() {
         return coursesCount;
     }
